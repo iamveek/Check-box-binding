3c02e91565729737eb74e66c50c0ad8ae01452b7
Muhammad Afaq Shahid Khan <97670374+iamveek@users.noreply.github.com>
2022-01-21 14:10:43 +0500

    Add files via upload

---
+++ src/components/Home.vue
@@ -0,0 +1,43 @@
+<template>
+
+<h1> Get input field value</h1>
+<input type="text" placeholder ="email"  v-model="email"/>
+<br/>
+<br/>
+<input type="password" placeholder ="password" v-model="password" />
+
+<br/>
+<br/>
+<button v-on:click="getData()" type="button">Click me</button>
+ </template>
+
+<script> 
+
+export default {
+    name: "Home",
+    data(){
+      return{
+        email:null,
+        password:null,
+      }
+    },
+    methods: {
+    getData()
+    {
+
+      console.warn("function called values are", this.email, this.password)
+
+    },
+    }
+
+   }
+
+
+  </script>
+  <style scoped> 
+  h1{
+  color: orange
+  }
+  
+  
+  </style>
